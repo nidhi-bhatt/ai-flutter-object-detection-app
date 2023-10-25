4af1935136663585b7af12bd0b4655d2aedabb0f
Nidhi Bhatt <itsnibhatt@gmail.com>
2023-10-25 13:30:35 +0530

    updated

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite/tflite.dart';
 
 class ScanController extends GetxController {
   @override
@@ -17,18 +21,49 @@ class ScanController extends GetxController {
 
   late CameraController cameraController;
   late List<CameraDescription> cameras;
+  late CameraImage cameraImage;
 
   var isCameraInitialised = false.obs;
+  var cameraCount = 0;
+  // gets images at 30 frames per second
+  //low end devices lagging issues
+  //get every third frame and pass it to the object
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
       cameraController = CameraController(cameras[0], ResolutionPreset.max);
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraCount++;
+        if (cameraCount % 10 == 0) {
+          cameraCount = 0;
+          cameraController.startImageStream((image) => objectDetector(image));
+          update();
+        }
+      });
 
       isCameraInitialised(true);
       update();
     } else {
       print("Permission Denied");
+    }
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+    if (detector != null) {
+      log("Result is $detector" as num);
     }
   }
 }
